7d839465f062259d4f91ab9cc85ba342b71b19a0
Minimalist-00 <kouki4122@icloud.com>
2023-06-25 00:17:11 +0900

    SignUpの処理を記述

--- usecase/user_usecase.go
+++ usecase/user_usecase.go
@@ -3,6 +3,8 @@ package usecase
 import (
 	"go-rest-api/model"
 	"go-rest-api/repository"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type IUserUsecase interface {
@@ -11,11 +13,31 @@ type IUserUsecase interface {
 }
 
 type userUsecase struct {
-	userRepo repository.IUserRepository
+	ur repository.IUserRepository
 }
 
-// func NewUserUsecase(ur repository.IUserRepository) IUserUsecase {
-// 	return &userUsecase{
-// 		userRepo: ur,
-// 	}
-// }
+func NewUserUsecase(ur repository.IUserRepository) IUserUsecase {
+	return &userUsecase{ur}
+}
+
+func (uu *userUsecase) SignUp(user model.User) (model.UserResponse, error) {
+	//パスワードのハッシュ化
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10) //GenerateFromPassword関数により、パスワードをハッシュ化
+	if err != nil {
+		return model.UserResponse{}, err
+	}
+	newUser := model.User{Email: user.Email, Password: string(hash)} //ハッシュ化したパスワードをnewUserに格納
+	if err := uu.ur.CreateUser(&newUser); err != nil {               //引数のnewUserをDBに保存
+		return model.UserResponse{}, err
+	}
+	resUser := model.UserResponse{
+		ID:    newUser.ID,
+		Email: newUser.Email,
+	} //レスポンス用のUserResponse型の変数を作成
+	return resUser, nil
+}
+
+func (uu *userUsecase) Login(user model.User) (string, error) {
+
+	return "", nil
+}
