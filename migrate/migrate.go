83910ad385e3ec2371c9d28629f10463956a32b2
Minimalist-00 <kouki4122@icloud.com>
2023-06-25 11:07:27 +0900

    ちょっとだけ

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -10,5 +10,5 @@ func main() {
 	dbConn := db.NewDB() //DB型のオブジェクトのアドレスを取得
 	defer fmt.Println("Successfully Migrated")
 	defer db.CloseDB(dbConn)
-	dbConn.AutoMigrate(&model.User{}, &model.Task{})
+	dbConn.AutoMigrate(&model.User{}, &model.Task{}) //DBに反映させたいモデル構造のアドレスを取得して渡す
 }
--- repository/user_repository.go
+++ repository/user_repository.go
@@ -1,8 +1,16 @@
 package repository
 
-import "go-rest-api/model"
+import (
+	"go-rest-api/model"
+
+	"gorm.io/gorm"
+)
 
 type IUserRepository interface {
 	GetUserByEmail(user *model.User, email string) error //引数のuserにemailをもつユーザーを格納｜返り値 エラーを返すときに使う
 	CreateUser(user *model.User) error                   //引数のuserをDBに保存
+}
+
+type userRepository struct {
+	db *gorm.DB
 }
