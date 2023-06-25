971cf73cc1a4c96bf2b9f5fc20885d090fcb2648
Minimalist-00 <kouki4122@icloud.com>
2023-06-25 16:30:53 +0900

    Update controller.go

--- controller/controller.go
+++ controller/controller.go
@@ -1,7 +1,9 @@
 package controller
 
 import (
+	"go-rest-api/model"
 	"go-rest-api/usecase"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
@@ -14,4 +16,22 @@ type IController interface {
 
 type UserController struct {
 	uu usecase.IUserUsecase
+}
+
+// usecaseを「Dependency Injection」するための関数（コンストラクタ）
+// usecaseのインスタンスを受け取る
+func NewUserController(uu usecase.IUserUsecase) IController {
+	return &UserController{uu}
+}
+
+func (uc *UserController) SignUp(c echo.Context) error {
+	user := model.User{}
+	if err := c.Bind(&user); err != nil { //リクエストボディをuserにバインド（User型に変換して格納）
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	userRes, err := uc.uu.SignUp(user) //usecaseのSignUpメソッドを呼び出し
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusCreated, userRes)
 }
