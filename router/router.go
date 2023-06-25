666f3ad284edfcba0114fe4b0b46a8a3db2f4c21
Minimalist-00 <kouki4122@icloud.com>
2023-06-25 16:59:44 +0900

    router.goの設定

--- router/router.go
+++ router/router.go
@@ -7,5 +7,9 @@ import (
 )
 
 func NewRouter(uc controller.IUserController) *echo.Echo {
-
+	e := echo.New()
+	e.POST("/signup", uc.SignUp)
+	e.POST("/login", uc.LogIn)
+	e.POST("/logout", uc.LogOut)
+	return e
 }
