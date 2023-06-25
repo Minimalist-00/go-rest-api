c7ead5a75dc1149cd0f0d9303c1f4b04b9d0eceb
Minimalist-00 <kouki4122@icloud.com>
2023-06-25 16:54:18 +0900

    Controller修正

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -10,7 +10,7 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-type IController interface {
+type IUserController interface {
 	SignUp(c echo.Context) error
 	LogIn(c echo.Context) error
 	LogOut(c echo.Context) error
---
+++ router/router.go
@@ -0,0 +1,11 @@
+package router
+
+import (
+	"go-rest-api/controller"
+
+	"github.com/labstack/echo/v4"
+)
+
+func NewRouter(uc controller.IUserController) *echo.Echo {
+
+}
