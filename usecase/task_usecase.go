104487442e67e2b2347d29927d8def9dd3290571
Minimalist-00 <kouki4122@icloud.com>
2023-06-27 21:27:11 +0900

    Update task_usecase.go

--- usecase/task_usecase.go
+++ usecase/task_usecase.go
@@ -1,6 +1,9 @@
 package usecase
 
-import "go-rest-api/model"
+import (
+	"go-rest-api/model"
+	"go-rest-api/repository"
+)
 
 type ITaskUsecase interface {
 	GetAllTasks(userId uint) ([]model.TaskResponse, error)
@@ -8,4 +11,79 @@ type ITaskUsecase interface {
 	CreateTask(task model.Task) (model.TaskResponse, error)
 	UpdateTask(task model.Task, userId uint, taskId uint) (model.TaskResponse, error)
 	DeleteTask(userId uint, taskId uint) error
+}
+
+type taskUsecase struct {
+	// repositoryのinterfaceに依存
+	tr repository.ITaskRepository //ITaskRepositoryを実装した構造体
+}
+
+func NewTaskUsecase(tr repository.ITaskRepository) ITaskUsecase {
+	return &taskUsecase{tr}
+}
+
+func (tu *taskUsecase) GetAllTasks(userId uint) ([]model.TaskResponse, error) {
+	tasks := []model.Task{}                                   //Taskの配列（スライス）を作成
+	if err := tu.tr.GetAllTasks(&tasks, userId); err != nil { //taskRepositoryのGetAllTasksを呼び出す
+		return nil, err
+	}
+	// 成功したとき↓
+	resTasks := []model.TaskResponse{} //TaskResponseの配列を作成
+	for _, v := range tasks {          //tasksの中身を1つずつ取り出す
+		t := model.TaskResponse{
+			ID:        v.ID,
+			Title:     v.Title,
+			CreatedAt: v.CreatedAt,
+			UpdatedAt: v.UpdatedAt,
+		}
+		resTasks = append(resTasks, t) //resTasksにmodel.TaskResponseを追加
+	}
+	return resTasks, nil
+}
+
+func (tu *taskUsecase) GetTaskById(userId uint, taskId uint) (model.TaskResponse, error) {
+	task := model.Task{}                                             //Taskの空の構造体を作成
+	if err := tu.tr.GetTaskById(&task, userId, taskId); err != nil { //空の構造体とuser・taskのIDを渡す
+		return model.TaskResponse{}, err
+	}
+	resTask := model.TaskResponse{
+		ID:        task.ID,
+		Title:     task.Title,
+		CreatedAt: task.CreatedAt,
+		UpdatedAt: task.UpdatedAt,
+	}
+	return resTask, nil
+}
+
+func (tu *taskUsecase) CreateTask(task model.Task) (model.TaskResponse, error) {
+	if err := tu.tr.CreateTask(&task); err != nil { //taskRepositoryのCreateTaskを呼び出す
+		return model.TaskResponse{}, err //TaskResponseの空の構造体とエラーを返す
+	}
+	resTask := model.TaskResponse{ //TaskResponseの構造体を作成
+		ID:        task.ID,
+		Title:     task.Title,
+		CreatedAt: task.CreatedAt,
+		UpdatedAt: task.UpdatedAt,
+	}
+	return resTask, nil
+}
+
+func (tu *taskUsecase) UpdateTask(task model.Task, userId uint, taskId uint) (model.TaskResponse, error) {
+	if err := tu.tr.UpdateTask(&task, userId, taskId); err != nil {
+		return model.TaskResponse{}, err
+	} //taskのアドレスが指すメモリのタスクが更新後の値に書き換わっている
+	resTask := model.TaskResponse{ //TaskResponseの構造体を作成
+		ID:        task.ID,
+		Title:     task.Title,
+		CreatedAt: task.CreatedAt,
+		UpdatedAt: task.UpdatedAt,
+	}
+	return resTask, nil
+}
+
+func (tu *taskUsecase) DeleteTask(userId uint, taskId uint) error {
+	if err := tu.tr.DeleteTask(userId, taskId); err != nil {
+		return err
+	}
+	return nil
 }
