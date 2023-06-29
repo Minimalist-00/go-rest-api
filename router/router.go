261940a44f495fcf365f74a8d5e20699c55c3d09
Minimalist-00 <kouki4122@icloud.com>
2023-06-29 19:35:44 +0900

    routerでcorsとcsrfのミドルウェアの設定

--- router/router.go
+++ router/router.go
@@ -2,15 +2,33 @@ package router
 
 import (
 	"go-rest-api/controller"
+	"net/http"
 	"os"
 
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func NewRouter(uc controller.IUserController, tc controller.ITaskController) *echo.Echo {
 	// ログイン関係のエンドポイントにの設定
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{ // CORSのミドルウェアの設定
+		AllowOrigins: []string{"http://localhost:3000", os.Getenv("FE_URL")}, // フロントエンドのURLを許可
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept,
+			echo.HeaderAccessControlAllowHeaders, echo.HeaderXCSRFToken},
+		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE"},
+		AllowCredentials: true,
+	}))
+	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{ // CSRFのミドルウェアの設定
+		CookiePath:     "/",
+		CookieDomain:   os.Getenv("API_DOMAIN"),
+		CookieHTTPOnly: true,
+		// CookieSameSite: http.SameSiteNoneMode,
+		CookieSameSite: http.SameSiteDefaultMode, // Postmanでのテスト用
+		// CookieMaxAge:   60,
+	}))
+
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.LogIn)
 	e.POST("/logout", uc.LogOut)
